85eb3c171c5af0588efe86e03a4424c43092658a
Azathoth-X <ranaprikshit25@gmail.com>
2025-06-23 00:23:43 +0530

    basic theme variance achieved

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { ModeToggle } from './components/mode-toggle.tsx'
+import { ThemeProvider } from './components/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider storageKey='vite-ui-theme'>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<ModeToggle/>}>
+            <Route path='/' element={<App/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
